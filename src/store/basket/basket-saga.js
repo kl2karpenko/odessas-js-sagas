2e244c1d3179b392cf6467b41de9b140cb59b137
lkarpenko <lkarpenko@alteryx.com>
2020-08-05 22:43:45 +0300

    update saga

--- src/store/basket/basket-saga.js
+++ src/store/basket/basket-saga.js
@@ -1,40 +1,7 @@
 import { all, put, call, take, fork, delay, cancel, actionChannel } from 'redux-saga/effects';
-import { channel } from 'redux-saga';
 import BasketAPI from './basket-api';
 
 const basketAPI = new BasketAPI();
-
-function* handleOrderProduct(requests) {
-  const { type, orderId, failedOrderId, successOrderId  } = yield take([
-    'BASKET_ORDER_CANCELED',
-    'BASKET_ORDER_SUCCEEDED',
-    'BASKET_ORDER_FAILED'
-  ]);
-
-  switch (type) {
-    case "BASKET_ORDER_CANCELED": {
-      const request = requests.get(orderId);
-      if (orderId && request) {
-        yield cancel(request);
-        requests.delete(orderId);
-      } else if (!orderId) {
-        yield all([...requests.values()].map(request => cancel(request)));
-        requests.clear();
-      }
-      break;
-    }
-    case "BASKET_ORDER_SUCCEEDED": {
-      requests.delete(successOrderId);
-      break;
-    }
-    case "BASKET_ORDER_FAILED": {
-      requests.delete(failedOrderId);
-      break;
-    }
-    default:
-      break;
-  }
-}
 
 function* createOrderBy({ orderId }) {
   try {
@@ -46,35 +13,11 @@ function* createOrderBy({ orderId }) {
   }
 }
 
-function* createSingleOrder(channel) {
-  const requests = new Map();
-
-  while (true) {
-    const { orderId } = yield take(channel);
-    const request = yield fork(createOrderBy, { orderId });
-    const prev = requests.get(orderId);
-    if (prev) {
-      requests.delete(orderId);
-      yield cancel(prev);
-    }
-    requests.set(orderId, request);
-
-    yield call(handleOrderProduct, requests);
-  }
-}
-
 function* watchQueuedOrders() {
-  // create a channel to queue incoming requests
-  const chan = yield call(channel)
-
-  // create 3 worker 'threads'
-  for (var i = 0; i < 3; i++) {
-    yield fork(createSingleOrder, chan)
-  }
-
+  const requestChan = yield actionChannel('BASKET_ORDER_REQUESTED')
   while (true) {
-    const { orderId } = yield take('BASKET_ORDER_REQUESTED')
-    yield put(chan, { orderId });
+    const { orderId } = yield take(requestChan);
+    yield call(createOrderBy, { orderId });
   }
 }
 
